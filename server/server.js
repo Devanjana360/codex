be9b5c7cbeef3a8cb295f06c4ce0afe763d2fd1b
Kumara@8828 <kumaraliyangea@gmail.com>
2023-02-09 04:44:27 +0530

    up date server file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 
@@ -13,7 +14,7 @@ const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(bodyParser.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
@@ -23,16 +24,16 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const { prompt, maxTokens, temperature, topP, presencePenalty, frequencyPenalty } = req.body;
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0, 
-      max_tokens: 4000, 
-      top_p: 1, 
-      frequency_penalty: 0.1, 
-      presence_penalty: 0, 
+      temperature: temperature || 0, 
+      max_tokens: maxTokens || 4000, 
+      top_p: topP || 1, 
+      frequency_penalty: frequencyPenalty || 0.1, 
+      presence_penalty: presencePenalty || 0, 
     });
 
     res.status(200).send({
@@ -45,4 +46,6 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))      
+
+/*  This version of the code adds support for additional parameters to the POST route, allowing clients to specify the maximum number of tokens, temperature, topP, presence penalty, and frequency penalty for the generated response. It also adds the body-parser library to enable parsing of the request body. */
